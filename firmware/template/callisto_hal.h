63cbeba851340aabadb4100d33a0452d4f19acf4
Tomash Ghz <ghz.tomash@hotmail.com>
2020-03-19 22:01:40 +0100

    licence headers

--- firmware/template/callisto_hal.h
+++ firmware/template/callisto_hal.h
@@ -1,3 +1,30 @@
+/* Callisto Eurorack Module - Hardware Abstraction Layer
+ * Copyright (c) 2019 Tomash Ghz
+ *
+ * Please support GHz Labs and open-source hardware by purchasing
+ * products directly from GHz Labs, donating or becoming a Github Sponsor!
+ * 
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef CallistoHAL_h
 #define CallistoHAL_h
 
